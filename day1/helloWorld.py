d67c3176a5d1f88462af4b1926da12b634748a51
djsmall <danjsmall@gmail.com>
2023-05-07 22:28:42 -0400

    using \n to seperate lines

--- day1/helloWorld.py
+++ day1/helloWorld.py
@@ -1,4 +1,2 @@
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+print("Day 1 - Python Print Function\n The Function is declared like this: \n print('what to print')")
